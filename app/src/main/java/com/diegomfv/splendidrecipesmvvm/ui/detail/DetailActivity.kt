a4c4aede3f27e4935c9d942b51c7136f3096f17e
diegomfv <diego.fajardo.villena@gmail.com>
2019-12-16 22:00:39 +0000

    Prepare DI (close #10)

--- app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/detail/DetailActivity.kt
@@ -1,3 +1,29 @@
 package com.diegomfv.splendidrecipesmvvm.ui.detail
 
-class DetailActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.diegomfv.splendidrecipesmvvm.R
+import org.koin.android.scope.currentScope
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val argument = "argument"
+    }
+
+    private val viewModel: DetailActivityViewModel by currentScope.viewModel(this) {
+        parametersOf(intent.getIntExtra(argument, -1))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_detail)
+
+//        viewModel.model.observe(this, Observer(::updateUi))
+    }
+
+}
--- app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/main/MainActivity.kt
@@ -3,8 +3,12 @@ package com.diegomfv.splendidrecipesmvvm.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.diegomfv.splendidrecipesmvvm.R
+import org.koin.android.scope.currentScope
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainActivityViewModel by currentScope.viewModel(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
