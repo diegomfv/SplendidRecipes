d4e4ff6f0c113ef311d6eb0d9c4ab79c68ed77fb
diegomfv <diego.fajardo.villena@gmail.com>
2020-01-19 20:23:15 +0000

    UI Update, added Event methods to UI, Details

--- app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/detail/DetailActivity.kt
@@ -5,6 +5,9 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.diegomfv.splendidrecipesmvvm.R
+import com.diegomfv.splendidrecipesmvvm.ui.common.startActivity
+import com.diegomfv.splendidrecipesmvvm.ui.common.toast
+import com.diegomfv.splendidrecipesmvvm.ui.main.MainActivityViewModel
 import org.koin.android.scope.currentScope
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
@@ -23,7 +26,26 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_detail)
 
-//        viewModel.model.observe(this, Observer(::updateUi))
+        viewModel.model.observe(this, Observer(::updateUi))
+        viewModel.event.observe(this, Observer { it.getContentIfNotHandled()?.let { event -> triggerEvent(event) } })
+
+    }
+
+
+    private fun updateUi (uiModel: DetailActivityViewModel.UiModel) {
+
+        when (uiModel) {
+            DetailActivityViewModel.UiModel.Loading -> toast("Loading...") //TODO
+            is DetailActivityViewModel.UiModel.Content -> toast("Show detail...") //TODO
+            is DetailActivityViewModel.UiModel.Error -> toast(uiModel.throwable.message ?: "Null Error") //TODO Add Error mapper
+        }
+    }
+
+    private fun triggerEvent (eventModel: DetailActivityViewModel.EventModel) {
+        when (eventModel) {
+            is DetailActivityViewModel.EventModel.Navigation -> startActivity<DetailActivity> {  }
+            DetailActivityViewModel.EventModel.RequestLocationPermission -> toast("Request location permissions")
+        }
     }
 
 }
--- app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/detail/DetailActivityViewModel.kt
+++ app/src/main/java/com/diegomfv/splendidrecipesmvvm/ui/detail/DetailActivityViewModel.kt
@@ -2,7 +2,8 @@ package com.diegomfv.splendidrecipesmvvm.ui.detail
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.diegomfv.splendidrecipesmvvm.data.model.dto.RecipeServerDTO
+import com.diegomfv.domain.Recipe
+import com.diegomfv.splendidrecipesmvvm.ui.common.Event
 import com.diegomfv.splendidrecipesmvvm.ui.common.ScopedViewModel
 import com.diegomfv.splendidrecipesmvvm.ui.common.logSth
 import kotlinx.coroutines.CoroutineDispatcher
@@ -14,34 +15,27 @@ class DetailActivityViewModel(
     private val _model = MutableLiveData<UiModel>()
     val model: LiveData<UiModel>
         get() {
-           if (_model.value == null) { logSth("_model triggered //") }
+            if (_model.value == null) {
+                logSth("_model triggered //")
+            }
             return _model
         }
 
+    val event = MutableLiveData<Event<EventModel>>()
+
+    fun onFavouriteClicked() {
+
+    }
+
     sealed class UiModel {
         object Loading : UiModel()
-        data class Content(val recipeServerDTOS: List<RecipeServerDTO>) : UiModel()
-        data class Navigation(val recipeServerDTO: RecipeServerDTO) : UiModel()
-        object RequestLocationPermission : UiModel()
-    }
-//
-//    private fun refresh() {
-//        _model.value = UiModel.RequestLocationPermission
-//    }
-//
-//
-//    fun onRecipeSelected(recipe: Recipe) {
-//        _model.value = UiModel.Navigation(recipe)
-//    }
-
-
-    fun onFavouriteClicked () {
-
-
-
+        data class Content(val recipes: List<Recipe>) : UiModel()
+        data class Error(val throwable: Throwable) : UiModel()
     }
 
-    override fun onCleared() {
-        super.onCleared()
+    sealed class EventModel {
+        data class Navigation(val recipe: Recipe) : EventModel()
+        object RequestLocationPermission : EventModel()
     }
+
 }
